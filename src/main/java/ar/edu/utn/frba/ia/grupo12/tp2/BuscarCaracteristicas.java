023043cc7af95356b85a720e84b8480ec1088daa
Alejandro D'Ambrosio <alejandro.dambrosio@gmail.com>
2015-10-07 00:06:11 -0300

    Corrección de toString y configuracion

--- src/main/java/ar/edu/utn/frba/ia/grupo12/tp2/BuscarCaracteristicas.java
+++ src/main/java/ar/edu/utn/frba/ia/grupo12/tp2/BuscarCaracteristicas.java
@@ -7,23 +7,25 @@ import main.java.ar.edu.utn.frba.ia.ag.ConfiguracionDefault;
 import main.java.ar.edu.utn.frba.ia.ag.Individuo;
 import main.java.ar.edu.utn.frba.ia.ag.cruzamiento.BinomialAzar;
 import main.java.ar.edu.utn.frba.ia.ag.mutacion.MutacionSimple;
+import main.java.ar.edu.utn.frba.ia.ag.paro.CantidadDeCiclos;
 import main.java.ar.edu.utn.frba.ia.ag.seleccion.Ranking;
 
 public class BuscarCaracteristicas {
-	static final int poblacionInicial = 999;
+	static final int poblacionInicial = 3000;
 
 	public static void main(String[] args) {
 		ConfiguracionDefault c = new ConfiguracionDefault();
 		
+		c.setCriterioDeParo(new CantidadDeCiclos(499L));
 		c.setPoblacionInicial(poblacionInicial);
 		c.setMetodoDeSeleccion(new Ranking(Math.round((float)(poblacionInicial * 1.5))));
-		c.setMutacion(new MutacionSimple(0.3));
+		c.setMutacion(new MutacionSimple(0.2));
 		c.setCruzamiento(new BinomialAzar());
 		
 		AlgoritmoGenetico buscaCaracteristicas = new AlgoritmoGenetico(c, ConjuntoCaracteristicas.class);
 		
 		Individuo caracteristicas = buscaCaracteristicas.ejecutar();
-		
+
 		Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe("Individuo final: " + caracteristicas.toString());
 	}
 
